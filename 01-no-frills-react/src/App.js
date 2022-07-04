520d4770ca5d0145fc6f39fa7a909293bae924a8
Julian Kang <julianswkang@gmail.com>
2022-07-04 10:47:00 -0400

    finished first section

--- 01-no-frills-react/src/App.js
+++ 01-no-frills-react/src/App.js
@@ -1,4 +1,5 @@
 const Pet = (props) => {
+  //the array will allow you to have multiple children react elements within this one react element
   return React.createElement("div", {}, [
     React.createElement("h1", {}, props.name),
     React.createElement("h2", {}, props.animal),
@@ -7,8 +8,9 @@ const Pet = (props) => {
 };
 
 const App = () => {
+  //can add properties into the curly brackets in order to provide one way data flow to children react elements
   return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement("h1", { id: "adopt-me-header" }, "Adopt Me!"),
     React.createElement(Pet, {
       name: "Luna",
       animal: "Dog",
